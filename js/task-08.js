32351e622d82f5e21164716558591941561bd322
Vladyslav <dyadyavadya308@gmail.com>
2023-03-21 20:18:35 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+//params ---
+const form = document.querySelector(".login-form");
 
+//events ---
+form.addEventListener("submit", handleSubmit);
+
+//functions ---
+function handleSubmit(event) {
+  event.preventDefault();
+  const emailValue = event.currentTarget.elements.email.value;
+  const passwordValue = event.currentTarget.elements.password.value;
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("all fields must be fill");
+  } else {
+    const formData = new FormData(event.currentTarget);
+
+    formData.forEach((value, name) => {
+      console.log(`name: ${name}, value: ${value}`);
+    });
+  }
+
+  event.currentTarget.reset();
+}
